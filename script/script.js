fd1dcf8de96c071b10d61ead987e517bcc4ca97f
Yehor Reva <yehorik85@gmail.com>
2020-03-31 15:30:00 +0300

    Lesson01 создал переменные money income addExpenses deposit mission period

---
+++ script/script.js
@@ -0,0 +1,25 @@
+let money;
+console.log('money: ', money);
+
+let income;
+console.log('income: ', income);
+
+let addExpenses;
+console.log('addExpenses: ', addExpenses);
+
+let deposit;
+console.log('deposit: ', deposit);
+
+let mission;
+console.log('mission: ', mission);
+
+let period;
+console.log('period: ', period);
+
+
+
+
+
+
+
+
